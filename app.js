af796a86d9fb81deeaaaad4ce63b2069739857d5
Jhonatan Ostroski <jhonatan-bruno@hotmail.com>
2023-07-02 10:52:13 -0300

    Teste Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const microfone = {
+    /*propriedade-valor do objeto, ou chave-valor do objeto*/
+    cor: 'preto',
+    marca: 'Fifine',
+    preco: 397.97
+}
+
+
+microfone.preco -= 200.0
+
+console.log(microfone.preco)
+
+/*
+Tipos de Dados em JS:
+
+Tipos primitivos:
+number = numero de 0 - 9, padrão é o decimal (float) 
+null
+undefined = vazio
+boolen
+string = qualquer caracter tabela Unicode
+bigint
+symbol
+
+Tipos Objeto: (entidade utilizada para agrupar características de algo)
+objetos literais {}
+arrays
+funções
+*/
